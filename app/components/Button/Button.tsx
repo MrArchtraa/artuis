c2488d3fefdcca21c43352951fb558cae7b21673
Andhika R. <archtraa.creative@gmail.com>
2024-04-23 11:21:34 +0700

    modifikasi komponen button

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import styled, { css } from 'styled-components';
+import { colors } from '../colors'; // Import objek warna dari file colors.ts
 
 // Definisikan tipe untuk prop variant
 type Variant = 'default' | 'outlined' | 'soft' | 'ghost';
@@ -8,72 +9,8 @@ type Variant = 'default' | 'outlined' | 'soft' | 'ghost';
 // Definisikan tipe untuk prop pada komponen tombol
 interface ButtonProps {
   variant: Variant;
-  color?: 'primary' | 'secondary' | 'success' | 'warning' | 'error' | 'dark' | 'info';
+  color?: keyof typeof colors; // Gunakan keyof typeof untuk memastikan bahwa color hanya bisa memiliki nilai yang ada di objek colors
 }
-
-const getSoftColor = (color: string) => {
-  switch (color) {
-    case 'primary':
-      return 'rgb(224 242 254)'; /* Warna biru */
-    case 'secondary':
-      return 'rgb(243 244 246)'; /* Warna orange */
-    case 'success':
-      return 'rgb(220 252 231)'; /* Warna hijau */
-    case 'warning':
-      return 'rgb(254 243 199)'; /* Warna kuning */
-    case 'error':
-      return 'rgb(254 226 226)'; /* Warna merah */
-    case 'dark':
-      return '#333'; /* Warna gelap */
-    case 'info':
-      return 'rgb(207 250 254)'; /* Warna cyan */
-    default:
-      return 'rgb(224 242 254)'; /* Warna biru sebagai default */
-  }
-}
-
-const getHoverSoftColor = (color: string) => {
-  switch (color) {
-    case 'primary':
-      return 'rgb(186 230 253)'; /* Warna biru */
-    case 'secondary':
-      return 'rgb(229 231 235)'; /* Warna orange */
-    case 'success':
-      return 'rgb(187 247 208)'; /* Warna hijau */
-    case 'warning':
-      return 'rgb(253 230 138)'; /* Warna kuning */
-    case 'error':
-      return 'rgb(254 202 202)'; /* Warna merah */
-    case 'dark':
-      return '#333'; /* Warna gelap */
-    case 'info':
-      return 'rgb(165 243 252)'; /* Warna cyan */
-    default:
-      return 'rgb(186 230 253)'; /* Warna biru sebagai default */
-  }
-}
-
-// Definisikan fungsi untuk mendapatkan warna berdasarkan prop color
-const getColor = (color: string) => {
-  switch (color) {
-    case 'primary':
-      return '#2196f3'; /* Warna biru */
-    case 'secondary':
-      return 'rgb(107 114 128)'; /* Warna orange */
-    case 'success':
-      return 'rgb(34 197 94)'; /* Warna hijau */
-    case 'warning':
-      return 'rgb(245 158 11)'; /* Warna kuning */
-    case 'error':
-      return 'rgb(239 68 68)'; /* Warna merah */
-    case 'dark':
-      return '#333'; /* Warna gelap */
-    case 'info':
-      return 'rgb(6 182 212)'; /* Warna cyan */
-    default:
-      return '#2196f3'; /* Warna biru sebagai default */
-  }
-};
 
 // Membuat komponen tombol dengan menggunakan Styled Components
 const Button = styled.button<ButtonProps>`
@@ -87,11 +24,11 @@ const Button = styled.button<ButtonProps>`
   ${(props) =>
     props.variant === 'default' &&
     css`
-      background-color: ${getColor(props.color || 'primary')};
+      background-color: ${colors[props.color || 'primary'].default};
       color: white;
 
       &:hover {
-        background-color: ${getColor(props.color || 'primary')};
+        background-color: ${colors[props.color || 'primary'].default};
       }
     `}
 
@@ -99,11 +36,11 @@ const Button = styled.button<ButtonProps>`
     props.variant === 'outlined' &&
     css`
       background-color: transparent;
-      border: 1px solid ${getColor(props.color || 'primary')};
-      color: ${getColor(props.color || 'primary')};
+      border: 1px solid ${colors[props.color || 'primary'].default};
+      color: ${colors[props.color || 'primary'].default};
 
       &:hover {
-        background-color: ${getColor(props.color || 'primary')};
+        background-color: ${colors[props.color || 'primary'].default};
         color: white;
       }
     `}
@@ -111,11 +48,11 @@ const Button = styled.button<ButtonProps>`
   ${(props) =>
     props.variant === 'soft' &&
     css`
-      background-color: ${getSoftColor(props.color || 'primary')};
-      color: ${getColor(props.color || 'primary')}; /* Warna teks sama dengan warna background */
+      background-color: ${colors[props.color || 'primary'].soft};
+      color: ${colors[props.color || 'primary'].default}; /* Warna teks sama dengan warna background */
 
       &:hover {
-        background-color: ${getHoverSoftColor(props.color || 'primary')}; /* Warna background saat dihover */
+        background-color: ${colors[props.color || 'primary'].hoverSoft}; /* Warna background saat dihover */
       }
     `}
 
@@ -123,10 +60,10 @@ const Button = styled.button<ButtonProps>`
     props.variant === 'ghost' &&
     css`
       background-color: transparent;
-      color: ${getColor(props.color || 'primary')};
+      color: ${colors[props.color || 'primary'].default};
 
       &:hover {
-        background-color: ${getSoftColor(props.color || 'primary')};
+        background-color: ${colors[props.color || 'primary'].soft};
       }
     `}
 `;
