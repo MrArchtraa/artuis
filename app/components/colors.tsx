ea4a635ee6a8a6107cceb867829fc73a6266c55f
Andhika R. <archtraa.creative@gmail.com>
2024-04-23 11:21:07 +0700

    membuat file warna

---
+++ app/components/colors.tsx
@@ -0,0 +1,40 @@
+// colors.ts
+
+export const colors = {
+    primary: {
+      default: '#2196f3',
+      soft: 'rgb(224 242 254)',
+      hoverSoft: 'rgb(186 230 253)'
+    },
+    secondary: {
+      default: 'rgb(107 114 128)',
+      soft: 'rgb(243 244 246)',
+      hoverSoft: 'rgb(229 231 235)'
+    },
+    success: {
+      default: 'rgb(34 197 94)',
+      soft: 'rgb(220 252 231)',
+      hoverSoft: 'rgb(187 247 208)'
+    },
+    warning: {
+      default: 'rgb(245 158 11)',
+      soft: 'rgb(254 243 199)',
+      hoverSoft: 'rgb(253 230 138)'
+    },
+    error: {
+      default: 'rgb(239 68 68)',
+      soft: 'rgb(254 226 226)',
+      hoverSoft: 'rgb(254 202 202)'
+    },
+    dark: {
+      default: '#333',
+      soft: '#333', // Sesuaikan dengan kebutuhan, karena warna gelap mungkin tidak memiliki variasi soft dan hover
+      hoverSoft: '#333' // Sesuaikan dengan kebutuhan, karena warna gelap mungkin tidak memiliki variasi soft dan hover
+    },
+    info: {
+      default: 'rgb(6 182 212)',
+      soft: 'rgb(207 250 254)',
+      hoverSoft: 'rgb(165 243 252)'
+    }
+  };
+  
