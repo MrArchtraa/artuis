55f04e3ca0f46050a0d4f26c5c4e2fc5246181a4
Andhika R. <archtraa.creative@gmail.com>
2024-04-23 11:11:02 +0700

    tes input dasar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import ArtButton from "./components/Button/Button";
+import ArtInput from "./components/Input/Input";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <ArtButton/>
+      <ArtInput/>
     </main>
   );
 }
